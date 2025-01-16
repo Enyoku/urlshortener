3186bc2c91320950ad2a810aa90966545a9d7228
Enyoku <mondex.2003@gmail.com>
2025-01-16 19:03:57 +0300

    Added Header Middleware

---
+++ servcached/pkg/api/middleware.go
@@ -0,0 +1,10 @@
+package api
+
+import "net/http"
+
+func headerMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json; charset=utf-8")
+		next.ServeHTTP(w, r)
+	})
+}
